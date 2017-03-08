7a01c4532b555d5db49adda888aa994d189fb29d
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-03-08 01:15:30 -0800

    asd

--- main.js
+++ main.js
@@ -2,9 +2,36 @@ window.sr = ScrollReveal({
     scale: 1,
     easing: "ease-out"
 });
-sr.reveal("h1",         {duration: 4000, delay: 100,  distance: "5px"});
-sr.reveal("h2",         {duration: 4000, delay: 1000, distance: "5px"});
-sr.reveal("h3",         {duration: 4200, delay: 1800, distance: "0"});
-sr.reveal("hr",         {duration: 4200, delay: 1800, distance: "0"});
-sr.reveal("blockquote", {duration: 5000, delay: 3000, distance: "50px"});
-sr.reveal(".dbox",      {duration: 6000, delay: 4000, distance: "0"});
+
+function setOpacity(elements, val) {
+    for (var i = 0; i < elements.length; i++) {
+        var els = document.querySelectorAll(elements[i]);
+        for (var j = 0; j < els.length; j++) {
+            els[j].style.opacity = val;
+        }
+    }
+}
+setOpacity(["h2","h3","hr","blockquote",".dbox"], 0);
+
+sr.reveal("h1", {duration: 4000, delay: 100, distance: "5px"});
+
+setTimeout(function() {
+    setOpacity(["h2"], 1);
+    sr.reveal("h2", {duration: 4000, distance: "5px"});
+}, 1000);
+
+setTimeout(function() {
+    setOpacity(["h3","hr"], 1);
+    sr.reveal("h3", {duration: 4200, distance: "0"});
+    sr.reveal("hr", {duration: 4200, distance: "0"});
+}, 1800);
+
+setTimeout(function() {
+    setOpacity(["blockquote"], 1);
+    sr.reveal("blockquote", {duration: 5000, distance: "50px"});
+}, 3000);
+
+setTimeout(function() {
+    setOpacity([".dbox"], 1);
+    sr.reveal(".dbox", {duration: 6000, distance: "0"});
+}, 4000);
