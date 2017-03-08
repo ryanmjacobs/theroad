17c4a94d470dcb79d55d4e8b7bb7559008c374b7
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-03-08 00:28:00 -0800

    dope af

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.sr = ScrollReveal({
     scale: 1,
     easing: "ease-out"
 });
-sr.reveal("h1", {duration: 4000, delay: 1000, distance: "5px"});
-sr.reveal("h2", {duration: 2000, delay: 0,    distance: "0"});
-sr.reveal("p",  {duration: 5000, delay: 1000, distance: "50px"});
+sr.reveal("h1",         {duration: 4000, delay: 1000, distance: "5px"});
+sr.reveal("h2",         {duration: 4000, delay: 1000, distance: "5px"});
+sr.reveal("h3",         {duration: 2000, delay: 0,    distance: "0"});
+sr.reveal("blockquote", {duration: 5000, delay: 1000, distance: "50px"});
