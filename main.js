13de6d1d87e44b48e9b2fded71ec5fe4a1694fb1
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-03-08 01:02:12 -0800

    asd

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ window.sr = ScrollReveal({
     scale: 1,
     easing: "ease-out"
 });
-sr.reveal("h1",         {duration: 4000, delay: 1000, distance: "5px"});
-sr.reveal("h2",         {duration: 4000, delay: 2000, distance: "5px"});
-sr.reveal("h3",         {duration: 4000, delay: 3000, distance: "0"});
-sr.reveal("hr",         {duration: 4000, delay: 3000, distance: "0"});
-sr.reveal("blockquote", {duration: 5000, delay: 4000, distance: "50px"});
-sr.reveal(".dbox",      {duration: 6000, delay: 5000, distance: "0"});
+sr.reveal("h1",         {duration: 4000, delay: 100,  distance: "5px"});
+sr.reveal("h2",         {duration: 4000, delay: 1000, distance: "5px"});
+sr.reveal("h3",         {duration: 4200, delay: 1800, distance: "0"});
+sr.reveal("hr",         {duration: 4200, delay: 1800, distance: "0"});
+sr.reveal("blockquote", {duration: 5000, delay: 3000, distance: "50px"});
+sr.reveal(".dbox",      {duration: 6000, delay: 4000, distance: "0"});
