2546767905ee38fc8bcd6e8e33e35122feff53a4
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-03-08 02:42:20 -0800

    clickyt

--- main.js
+++ main.js
@@ -36,3 +36,52 @@ setTimeout(function() {
     setOpacity([".dbox"], 1);
     sr.reveal(".dbox", {duration: 6000, distance: "0"});
 }, 4000);
+
+///////
+
+// data = ["header", "body"]
+function render_box(data) {
+    var head_el = document.getElementsByClassName("dbox-word")[0];
+    var body_el = document.getElementsByClassName("dbox-definition")[0];
+
+    head_el.innerHTML = data[0];
+    body_el.innerHTML = data[1];
+}
+
+function get_text(key) {
+    var k = (key.split(" ")[0]);
+
+    if (k == "mistrusted")
+        return ["Mistrusted", "This word contributes to the weary tone of the passage and the novel as a whole. He doesn’t trust things, even good things like pleasant dreams."];
+    else if (k == "peril")
+        return ["Peril", "This adds to the threatened tone of the novel. There are very few instances of safety and comfort."];
+    else if (k == "languor")
+        return ["Languor and Death", "These words remind the reader of the ever present threat of death and the tone of nothingness and blackness."];
+    else if (k == "if")
+        return ["If he lived ... would all be lost", "The phrasing of this sentence brings about the theme of sadness and uncertainty. There is the question of how long he will live, and how the world will fare during this post apocalyptic time. The sadness comes in when he talks of all of the world being lost. Everything will fade."];
+    else if (k == "blind")
+        return ["Blind", "Throughout the novel, the theme of sight and blindness is prevalent. The tone here is loss and sadness. He often feels like his life and journey are lacking direction. He can’t seem to see the way in front of him."];
+    else if (k == "fading")
+        return ["slowly fading", "The tone here is of desolation. The world they knew is fading; all that remains is the dreams, and even those are fading. Nothing good and pure remains, except memories, and all of our memories fail us eventually."];
+    else if (k == "phantom")
+        return ["Phantom", "The ghost of an old world still haunts the man and death lingers everywhere in this landscape."];
+}
+
+var tone_click = function() {
+    var txt = get_text(this.innerHTML);
+    render_box(txt);
+};
+var tone_mouseover = function() {
+    console.log(this);
+};
+var tone_mouseout = function() {
+    console.log(this);
+};
+
+var tone_els = document.getElementsByClassName("tone");
+for (var i = 0; i < tone_els.length; i++ ) {
+    var el = tone_els[i];
+    el.onclick = tone_click;
+    el.onmouseover = tone_mouseover;
+    el.onmouseout  = tone_mouseout;
+}
