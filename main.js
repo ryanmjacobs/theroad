695b30bfc75e23fa0b279e67f5de8231c7f49b9b
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-03-08 00:03:43 -0800

    asd

---
+++ main.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({
+    scale: 1,
+    easing: "ease-out"
+});
+sr.reveal("h1", {duration: 4000, delay: 1000, distance: "5px"});
+sr.reveal("h2", {duration: 2000, delay: 0,    distance: "0"});
+sr.reveal("p",  {duration: 5000, delay: 1000, distance: "50px"});
