d9d536bebaec5960d5796c522da2a80c44bdb87e
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-03-08 01:00:30 -0800

    fade

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ window.sr = ScrollReveal({
     easing: "ease-out"
 });
 sr.reveal("h1",         {duration: 4000, delay: 1000, distance: "5px"});
-sr.reveal("h2",         {duration: 4000, delay: 1000, distance: "5px"});
-sr.reveal("h3",         {duration: 2000, delay: 0,    distance: "0"});
-sr.reveal("blockquote", {duration: 5000, delay: 1000, distance: "50px"});
+sr.reveal("h2",         {duration: 4000, delay: 2000, distance: "5px"});
+sr.reveal("h3",         {duration: 4000, delay: 3000, distance: "0"});
+sr.reveal("hr",         {duration: 4000, delay: 3000, distance: "0"});
+sr.reveal("blockquote", {duration: 5000, delay: 4000, distance: "50px"});
+sr.reveal(".dbox",      {duration: 6000, delay: 5000, distance: "0"});
